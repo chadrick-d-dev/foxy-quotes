24b917c5ee371fbf59935d09721f0c965dcd058d
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-13 19:25:53 -0700

    Build app component

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NaviBar from '../NaviBar/NaviBar';
+import GeneratorContainer from '../GeneratorContainer/GeneratorContainer';
+import SavedContainer from '../SavedContainer/SavedContainer';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-function App() {
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#78909c',
+    },
+    secondary: {
+      main: '#00acc1',
+    },
+  },
+});
+
+const App = () => {
   return (
+  <MuiThemeProvider theme={theme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NaviBar/>
+      <Switch>
+        <Redirect exact path="/" to="/foxyquoter" component={About}/>
+        <Route path="/foxy-quoter" component={GeneratorContainer}/>
+        <Route path="/quotie-foxes" component={SavedContainer}/>
+      </Switch>
     </div>
+    </MuiThemeProvider>
   );
 }
-
 export default App;
