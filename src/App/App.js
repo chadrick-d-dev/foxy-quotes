dd84d7d876dd343226745f847cc423574da7226f
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-13 21:47:49 -0700

    Create structure and methods for app component

--- src/App/App.js
+++ src/App/App.js
@@ -3,31 +3,42 @@ import NaviBar from '../NaviBar/NaviBar';
 import GeneratorContainer from '../GeneratorContainer/GeneratorContainer';
 import SavedContainer from '../SavedContainer/SavedContainer';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#78909c',
-    },
-    secondary: {
-      main: '#00acc1',
-    },
-  },
-});
 
 const App = () => {
+  const [foxyQuote, setFoxyQuote] = useState(null);
+  const [savedFoxyQuotes, setSavedFoxyQuotes] = useState();
+
+  const getFoxyQuoteInfo = (foxPhoto, quote, author) => {
+    setFoxyQuote = { 
+      "id": Date.now(),
+      "img": foxPhoto, 
+      "quote": quote, 
+      "author": author
+    }
+  }
+
+  const saveFoxyQuote = (foxyQuote) => {
+    setSavedFoxyQuotes(savedFoxyQuotes => [...savedFoxyQuotes, foxyQuote])
+  }
+
+  const deleteFoxyQuote = (event) => {
+    const id = event.target.getAttribute("id")
+    setSavedFoxyQuotes(savedFoxyQuotes.filter(quote => quote.id !== id))
+  } 
+
   return (
-  <MuiThemeProvider theme={theme}>
-    <div className="App">
-      <NaviBar/>
-      <Switch>
-        <Redirect exact path="/" to="/foxyquoter" component={About}/>
-        <Route path="/foxy-quoter" component={GeneratorContainer}/>
-        <Route path="/quotie-foxes" component={SavedContainer}/>
-      </Switch>
-    </div>
-    </MuiThemeProvider>
+  <div className="App">
+    <NaviBar/>
+    <Switch>
+      <Redirect exact path="/" to="/foxy-quoter" component={About}/>
+      <Route path="/foxy-quoter" 
+        render={() => <GeneratorContainer foxyQuote={foxyQuote} getFoxyQuoteInfo={getFoxyQuoteInfo} saveFoxyQuote={saveFoxyQuote}/>}
+      />
+      <Route path="/quotie-foxes" 
+        render={() => <SavedContainer savedFoxyQuotes={savedFoxyQuotes} deleteFoxyQuote={deleteFoxyQuote}/>}
+      />
+    </Switch>
+  </div>
   );
 }
 export default App;
