9d45b4cbe55e8b98ddcdddbf9422628f5d9cd055
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-13 19:08:13 -0700

    create api calls for getFoxPicture and getQuote

---
+++ src/apiCallls.js
@@ -0,0 +1,13 @@
+export const getFoxPicture = () => {
+  return (
+  fetch(`https://randomfox.ca/floof/`)
+  .then(response => response.json())
+  );
+}
+
+export const getQuote = () => {
+  return (
+  fetch(`https://api.quotable.io/random?tags=inspirational`)
+  .then(response => response.json())
+  );
+}
