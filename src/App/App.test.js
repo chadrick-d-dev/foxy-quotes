40ca8d369305411d1bfe2d44bdaf7e9f06d0965b
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-13 17:12:21 -0700

    Set up test file for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,24 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+import { MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <App/>
+      </MemoryRouter>
+    )
+  })
+  it.skip('should render', () => {
+    // setup
+
+    // assertions
+
+    //expectation
+
+  })
+})
