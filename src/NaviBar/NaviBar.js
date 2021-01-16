6a24f6cbfc21629c4871c08dc1f809bd112fb42b
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-16 15:39:04 -0700

    Get the links to render

--- src/NaviBar/NaviBar.js
+++ src/NaviBar/NaviBar.js
@@ -5,11 +5,11 @@ const NaviBar = () => {
 
   return (
     <div>
-      <Link label='Foxy Quoter' path='/foxy-quoter'/>
-      <Link label='Foxy Stash' path='/quotie-foxes'/>
-      <div>
+      <Link to='/foxy-quoter' className='foxyQuoter'>Foxy Quoter</Link>
+      <Link to='/quotie-foxes' className='foxyStash'>Foxy Stash</Link>
+      {/* <div>
         <img className='logo' src={foxyquoteslogo} alt='Foxy Quotes Logo'/>
-      </div>
+      </div> */}
     </div>
   )
 }
