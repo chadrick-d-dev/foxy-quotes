1ecd294f0e186f047da8fe56caba3ba788771f0d
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-16 13:24:26 -0700

    Move methods & setStates to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 import NaviBar from '../NaviBar/NaviBar';
 import GeneratorContainer from '../GeneratorContainer/GeneratorContainer';
 import SavedContainer from '../SavedContainer/SavedContainer';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { getFoxPhoto, getQuote } from '../apiCalls.js';
 
 const App = () => {
-  const [foxyQuote, setFoxyQuote] = useState(null);
-  const [savedFoxyQuotes, setSavedFoxyQuotes] = useState();
+  const [foxPhoto, setFoxPhoto ] = useState('');
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('')
+  const [foxyQuote, setFoxyQuote] = useState('');
+  const [savedFoxyQuotes, setSavedFoxyQuotes] = useState([]);
+  
+  const obtainFoxPhoto = async () => {
+    await getFoxPhoto()
+    .then(data => setFoxPhoto(data.image))
+    .catch(err => console.log(err))
+  }
+
+  const obtainQuote = async () => {
+    await getQuote()
+    .then(data =>  {
+      setQuote(data.content)  
+      setAuthor(data.author)
+    })
+    .catch(err => console.log(err))
+  }
 
   const getFoxyQuoteInfo = (foxPhoto, quote, author) => {
-    setFoxyQuote = { 
+    setFoxyQuote({ 
       "id": Date.now(),
       "img": foxPhoto, 
       "quote": quote, 
       "author": author
-    }
+    })
+  }
+
+  const createFoxyQuote = () => {
+    obtainFoxPhoto()
+    obtainQuote()
+    getFoxyQuoteInfo(foxPhoto, quote, author)
   }
 
   const saveFoxyQuote = (foxyQuote) => {
@@ -26,13 +52,15 @@ const App = () => {
     setSavedFoxyQuotes(savedFoxyQuotes.filter(quote => quote.id !== id))
   } 
 
+  
+
   return (
   <div className="App">
     <NaviBar/>
     <Switch>
-      <Redirect exact path="/" to="/foxy-quoter" component={About}/>
+      <Redirect exact path="/" to="/foxy-quoter" component={GeneratorContainer}/>
       <Route path="/foxy-quoter" 
-        render={() => <GeneratorContainer foxyQuote={foxyQuote} getFoxyQuoteInfo={getFoxyQuoteInfo} saveFoxyQuote={saveFoxyQuote}/>}
+      render={() => <GeneratorContainer createFoxyQuote={createFoxyQuote} foxyQuote={foxyQuote} getFoxyQuoteInfo={getFoxyQuoteInfo} saveFoxyQuote={saveFoxyQuote}/>}
       />
       <Route path="/quotie-foxes" 
         render={() => <SavedContainer savedFoxyQuotes={savedFoxyQuotes} deleteFoxyQuote={deleteFoxyQuote}/>}
--- src/GeneratorContainer/GeneratorContainer.js
+++ src/GeneratorContainer/GeneratorContainer.js
@@ -1,56 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { getFoxPhoto, getQuote } from '../apiCalls';
+import React, { useState } from 'react';
 
-const GeneratorContainer = ({getFoxyQuoteInfo, foxyQuote, saveFoxyQuote}) => {
+
+const GeneratorContainer = ({getFoxyQuoteInfo, createFoxyQuote, foxyQuote, saveFoxyQuote}) => {
 
   const [foxPhoto, setFoxPhoto ] = useState('');
-  const [quote, setQuote] = useState('');
-  const [author, setAuthor] = useState('')
-  const [foxyCard, setFoxyCard] = useState(false);
-  useEffect(() => {
-  }, [])
+  const [quote, setQuote] = useState({});
+  // const [foxyCard, setFoxyCard] = useState(false);
+  // useEffect(() => {
+  // }, [])
 
-  const obtainFoxPhoto = async () => {
-    await getFoxPhoto()
-    .then(data => setFoxPhoto(data.image))
-    .catch(err => console.log(err))
-  }
-
-  const obtainQuote = async () => {
-    await getQuote()
-    .then(data => { 
-      setQuote(data.content)
-      setAuthor(data.author)
-    })
-    .catch(err => console.log(err))
-  }
-
-  handleClick = () => {
-    obtainFoxPhoto()
-    obtainQuote()
-    getFoxyQuoteInfo(foxPhoto, quote, author)
-    setFoxyCard(true)
-  }
+  
   //I need to conditional render solo button for new foxyquote if there is no foxyQuoteInfo,
   //If there is foxyquoteinfo I need to render a card with save button and refresh button
   //if refresh button is clicked a new foxy button will appear
   if (!foxyQuote.length) {
     return (
       <div>
-        <button name='create-button' alt='Foxy Quote create button' onClick={handleClick}>Foxy Me A Quote!</button>
+        <button name='create-button' alt='Foxy Quote create button' onClick={createFoxyQuote}>Foxy Me A Quote!</button>
       </div> 
     )
   } else {
     return (
       <div>
         <div name='new-foxy-quote-card'>
-          <img src={foxyQuote.img} />
+          <img src={foxyQuote.img} alt='Fox'/>
           <div>
             <p label='Quote:' name='quote'>{foxyQuote.quote}</p>
             <p label='Author:' name='author'>{foxyQuote.author}</p>
           </div>
           <button name='save-button' alt='Foxy Quote save button' onClick={saveFoxyQuote}>Save This Trickster!</button>
-          <button name='create-more-button' alt='Foxy Quote create button' onClick={handleClick}>Fox Me Another Quote!</button>
+          <button name='create-more-button' alt='Foxy Quote create button' onClick={createFoxyQuote}>Fox Me Another Quote!</button>
         </div>
       </div>
     )
