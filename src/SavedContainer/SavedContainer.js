51bfc773b106b7005d8ac5c36225fae63c49b72d
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-16 17:25:19 -0700

    Create conditional render in SavedContainer

--- src/SavedContainer/SavedContainer.js
+++ src/SavedContainer/SavedContainer.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
-const SavedContainer = ({saveFoxyQuotes, deleteFoxyQuote}) => {
-  return (
-    <div>hi</div>
-  )
-
+const SavedContainer = ({savedFoxyQuotes, deleteFoxyQuote}) => {
+  if (savedFoxyQuotes.length < 1) {
+    return (
+      <section>
+         <h1 className="no-foxy-quotes">You haven't saved any Foxy Quotes to your stash yet!</h1>
+      </section>
+    )
+  } else if (savedFoxyQuotes.length > 0) {
+    const displaySavedFoxes = savedFoxyQuotes.map(foxyQuote => {
+      return (
+        <div className='saved-foxy-quote'>
+          <img src={foxyQuote.img} alt='Fox'/>
+          <div>
+            <p label='Quote:' name='quote'>{foxyQuote.quote}</p>
+            <p label='Author:' name='author'>{foxyQuote.author}</p>
+          </div>
+          <button name='delete-button' id={foxyQuote.id} alt='Foxy Quote delete button' onClick={deleteFoxyQuote}>Delete This Trickster!</button>
+        </div>
+      )
+    })
+    return(
+      <section>
+        {displaySavedFoxes}
+      </section>
+    )
+  }
 }
 
 export default SavedContainer;
