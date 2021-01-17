589409ac09951b96e304b78398d5ba5227f40fd8
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-16 17:54:22 -0700

    Refactor tags and add classNames

--- src/SavedContainer/SavedContainer.js
+++ src/SavedContainer/SavedContainer.js
@@ -3,25 +3,27 @@ import React from 'react';
 const SavedContainer = ({savedFoxyQuotes, deleteFoxyQuote}) => {
   if (savedFoxyQuotes.length < 1) {
     return (
-      <section>
-         <h1 className="no-foxy-quotes">You haven't saved any Foxy Quotes to your stash yet!</h1>
+      <section className='no-quotes-message'>
+         <h1 className="no-quotes-header">You haven't saved any Foxy Quotes to your stash yet!</h1>
       </section>
     )
   } else if (savedFoxyQuotes.length > 0) {
     const displaySavedFoxes = savedFoxyQuotes.map(foxyQuote => {
       return (
-        <div className='saved-foxy-quote'>
-          <img src={foxyQuote.img} alt='Fox'/>
-          <div>
+        <section className='saved-foxy-quote'>
+          <div className='fox-image'>
+            <img src={foxyQuote.img} alt='Fox'/>
+          </div>
+          <div className="fox-text">
             <p label='Quote:' name='quote'>{foxyQuote.quote}</p>
             <p label='Author:' name='author'>{foxyQuote.author}</p>
           </div>
           <button name='delete-button' id={foxyQuote.id} alt='Foxy Quote delete button' onClick={deleteFoxyQuote}>Delete This Trickster!</button>
-        </div>
+        </section>
       )
     })
     return(
-      <section>
+      <section className='saved-container'>
         {displaySavedFoxes}
       </section>
     )
