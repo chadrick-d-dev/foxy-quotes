bb21e503b107ff7e21ce035e7e07447e42681447
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-17 12:01:08 -0700

    Add alt tags to logo images

--- src/NotFoundPage/NotFoundPage.js
+++ src/NotFoundPage/NotFoundPage.js
@@ -8,9 +8,9 @@ const NotFoundPage = () => {
   return (
     <section className='not-found-page'>
       <div className='lost-message-box'>
-        <img className='lost-logo' src={FoxyLogoOrange}/>
+        <img className='lost-logo' src={FoxyLogoOrange} alt='Foxy Quotes Logo'/>
         <p className='lost-p-one'>The 2 dimensional fox ponders... <br/> Are you lost?</p>
-        <img className='lost-image' src={PageNotFound}/>
+        <img className='lost-image' src={PageNotFound} alt='Questioning Fox'/>
         <Link to="/foxy-quoter" className='lost-link'>Go back to Foxy Quotes</Link>
       </div>
     </section>
