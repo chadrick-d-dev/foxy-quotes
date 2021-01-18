639529cb5930198388f112eecb1a5526c6401629
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-17 17:50:11 -0700

    Write and pass tests for NaviBar

--- src/NaviBar/NaviBar.test.js
+++ src/NaviBar/NaviBar.test.js
@@ -2,23 +2,54 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import NaviBar from './NaviBar';
-import { MemoryRouter } from 'react-router';
+import { Router, MemoryRouter } from 'react-router';
 import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from "history";
 
 describe('NaviBar', () => {
-  beforeEach(() => {
+  it('should render NaviBar with foxy quoter and foxy stash tabs', () => {
     render(
       <MemoryRouter>
         <NaviBar/>
       </MemoryRouter>
     )
+    const quoterTab = screen.getByText('Foxy Quoter')
+    const stashTab = screen.getByText('Foxy Stash')
+    expect(quoterTab).toBeInTheDocument()
+    expect(stashTab).toBeInTheDocument()
   })
-  it.skip('should render', () => {
-    // setup
 
-    // assertions
+  it('should render NaviBar with a logo', () => {
+    render(
+      <MemoryRouter>
+        <NaviBar/>
+      </MemoryRouter>
+    )
+    const logo = screen.queryByAltText('Foxy Quotes Logo')
+    expect(logo).toBeInTheDocument()
+  })
+  
+  it('should route user to Foxy Quoter page when Foxy Quoter tab is clicked', () => {
+    const history = createMemoryHistory()
+    render(
+      <Router history={history}>
+        <NaviBar/>
+      </Router>
+    )
+    const quoterTab = screen.getByText('Foxy Quoter')
+    userEvent.click(quoterTab)
+    expect(history.location.pathname).toBe('/foxy-quoter')
+  })
 
-    //expectation
-
+  it('should route user to Foxy Stash page when Foxy Stash tab is clicked', () => {
+    const history = createMemoryHistory()
+    render(
+      <Router history={history}>
+        <NaviBar/>
+      </Router>
+    )
+    const stashTab = screen.getByText('Foxy Stash')
+    userEvent.click(stashTab)
+    expect(history.location.pathname).toBe('/foxy-stash')
   })
 })
